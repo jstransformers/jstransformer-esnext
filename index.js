9ecdc62e7005d205da1edf3a62ec8f8d20385d5d
Caleb Eby <calebeby@users.noreply.github.com>
2017-01-05 09:58:01 -0800

    Add inputformats

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var esnext = require('esnext')
 
 exports.name = 'esnext'
 exports.outputFormat = 'js'
-
+exports.inputFormats = ['js', 'esnext']
 exports.render = function (str, options) {
   var out = esnext.convert(str, options)
   return out.code
